38e78a06fc9963f267d97e7983000a903ab3a314
Lucas I <lisselhard@aol.de>
2019-06-10 03:42:45 +0200

    Add files via upload

---
+++ ID-Picker/ID-Picker/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace ID_Picker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 76);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(775, 393);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(12, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(518, 25);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(665, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button2.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 30);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Monster";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button3.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(12, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 30);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Item";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button4.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(174, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 30);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Map";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.ForeColor = System.Drawing.Color.Black;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Deutsch",
+            "Englisch"});
+            this.comboBox1.Location = new System.Drawing.Point(665, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 25);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button5.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.button5.ForeColor = System.Drawing.Color.Black;
+            this.button5.Location = new System.Drawing.Point(537, 44);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(122, 30);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Filter";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(799, 481);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ID-Picker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ ID-Picker/ID-Picker/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ID_Picker
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, string> monster = new Dictionary<string, string>();
+        private int currentList = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedItem = comboBox1.Items[0];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int curIndex = 0;
+                if (dataGridView1.SelectedRows.Count > 0)
+                {
+                    curIndex = dataGridView1.SelectedRows[0].Index;
+                }
+                dataGridView1.ClearSelection();
+
+                for (int i = curIndex + 1; i < dataGridView1.Rows.Count - 1; i++)
+                {
+                    if (dataGridView1["Name", i].Value.ToString().ToUpper().Contains(textBox1.Text.ToUpper()))
+                    {
+                        dataGridView1.Rows[i].Selected = true;
+                        dataGridView1.FirstDisplayedScrollingRowIndex = i;
+                        return;
+                    }
+                }
+                for (int i = 0; i < curIndex + 1; i++)
+                {
+                    if (dataGridView1["Name", i].Value.ToString().ToUpper().Contains(textBox1.Text.ToUpper()))
+                    {
+                        dataGridView1.Rows[i].Selected = true;
+                        dataGridView1.FirstDisplayedScrollingRowIndex = i;
+                        return;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "button1_Click", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            currentList = 2;
+            try
+            {
+                dataGridView1.Columns.Clear();
+                dataGridView1.Rows.Clear();
+
+                dataGridView1.Columns.Add("ID", "ID");
+                dataGridView1.Columns.Add("Name", "Name");
+                dataGridView1.Columns[0].Width = 100;
+                dataGridView1.Columns[1].Width = dataGridView1.Width - 100 - 60;
+
+                if (comboBox1.Items[comboBox1.SelectedIndex].ToString() == "Deutsch")
+                {
+                    foreach (string s in Properties.Resources.DE_monster.Split('\n'))
+                    {
+                        dataGridView1.Rows.Add(s.Split('\t'));
+                    }
+                }
+                if (comboBox1.Items[comboBox1.SelectedIndex].ToString() == "Englisch")
+                {
+                    foreach (string s in Properties.Resources.EN_monster.Split('\n'))
+                    {
+                        dataGridView1.Rows.Add(s.Split('\t'));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "button1_Click", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            currentList = 1;
+            try
+            {
+                dataGridView1.Columns.Clear();
+                dataGridView1.Rows.Clear();
+
+                dataGridView1.Columns.Add("ID", "ID");
+                dataGridView1.Columns.Add("Name", "Name");
+                dataGridView1.Columns.Add("Level", "Level");
+                dataGridView1.Columns.Add("Type", "Type");
+                dataGridView1.Columns[0].Width = 100;
+                dataGridView1.Columns[1].Width = dataGridView1.Width - 300 - 60;
+                dataGridView1.Columns[2].Width = 100;
+                dataGridView1.Columns[3].Width = 100;
+
+                if (comboBox1.Items[comboBox1.SelectedIndex].ToString() == "Deutsch")
+                {
+                    foreach (string s in Properties.Resources.DE_items.Split('\n'))
+                    {
+                        dataGridView1.Rows.Add(s.Split('\t'));
+                    }
+                }
+                if (comboBox1.Items[comboBox1.SelectedIndex].ToString() == "Englisch")
+                {
+                    foreach (string s in Properties.Resources.EN_items.Split('\n'))
+                    {
+                        dataGridView1.Rows.Add(s.Split('\t'));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "button1_Click", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            currentList = 3;
+            try
+            {
+                dataGridView1.Columns.Clear();
+                dataGridView1.Rows.Clear();
+
+                dataGridView1.Columns.Add("ID", "ID");
+                dataGridView1.Columns.Add("Name", "Name");
+                dataGridView1.Columns[0].Width = 100;
+                dataGridView1.Columns[1].Width = dataGridView1.Width - 100 - 60;
+
+                if (comboBox1.Items[comboBox1.SelectedIndex].ToString() == "Deutsch")
+                {
+                    foreach (string s in Properties.Resources.DE_maps.Split('\n'))
+                    {
+                        dataGridView1.Rows.Add(s.Split('\t'));
+                    }
+                }
+                if (comboBox1.Items[comboBox1.SelectedIndex].ToString() == "Englisch")
+                {
+                    foreach (string s in Properties.Resources.EN_maps.Split('\n'))
+                    {
+                        dataGridView1.Rows.Add(s.Split('\t'));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "button1_Click", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (currentList)
+            {
+                case 1:
+                    button3_Click(sender, e);
+                    break;
+                case 2:
+                    button2_Click(sender, e);
+                    break;
+                case 3:
+                    button4_Click(sender, e);
+                    break;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+
+            switch (currentList)
+            {
+                case 1:
+                    if (comboBox1.Items[comboBox1.SelectedIndex].ToString() == "Deutsch")
+                    {
+                        foreach (string s in Properties.Resources.DE_items.Split('\n').Where(x => x.ToUpper().Contains(textBox1.Text.ToUpper())))
+                        {
+                            dataGridView1.Rows.Add(s.Split('\t'));
+                        }
+                    }
+                    if (comboBox1.Items[comboBox1.SelectedIndex].ToString() == "Englisch")
+                    {
+                        foreach (string s in Properties.Resources.EN_items.Split('\n').Where(x => x.ToUpper().Contains(textBox1.Text.ToUpper())))
+                        {
+                            dataGridView1.Rows.Add(s.Split('\t'));
+                        }
+                    }
+                    break;
+                case 2:
+                    if (comboBox1.Items[comboBox1.SelectedIndex].ToString() == "Deutsch")
+                    {
+                        foreach (string s in Properties.Resources.DE_monster.Split('\n').Where(x => x.ToUpper().Contains(textBox1.Text.ToUpper())))
+                        {
+                            dataGridView1.Rows.Add(s.Split('\t'));
+                        }
+                    }
+                    if (comboBox1.Items[comboBox1.SelectedIndex].ToString() == "Englisch")
+                    {
+                        foreach (string s in Properties.Resources.EN_monster.Split('\n').Where(x => x.ToUpper().Contains(textBox1.Text.ToUpper())))
+                        {
+                            dataGridView1.Rows.Add(s.Split('\t'));
+                        }
+                    }
+                    break;
+                case 3:
+                    if (comboBox1.Items[comboBox1.SelectedIndex].ToString() == "Deutsch")
+                    {
+                        foreach (string s in Properties.Resources.DE_maps.Split('\n').Where(x => x.ToUpper().Contains(textBox1.Text.ToUpper())))
+                        {
+                            dataGridView1.Rows.Add(s.Split('\t'));
+                        }
+                    }
+                    if (comboBox1.Items[comboBox1.SelectedIndex].ToString() == "Englisch")
+                    {
+                        foreach (string s in Properties.Resources.EN_maps.Split('\n').Where(x => x.ToUpper().Contains(textBox1.Text.ToUpper())))
+                        {
+                            dataGridView1.Rows.Add(s.Split('\t'));
+                        }
+                    }
+                    break;
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == 13)
+            {
+                button1_Click(sender, e);
+            }
+        }
+    }
+}
